ba29182ed6b4b55a8438bc60d71a8bdd518411c6
Jeremy Beuvry <jeremy.beuvry@epita.fr>
2017-07-25 20:08:44 +0200

    OwnLineBrace pass all test

--- src/rules/brace.rs
+++ src/rules/brace.rs
@@ -16,11 +16,14 @@ impl Rule for OwnLineBrace {
 		let mut line_number: usize = 1;
 
 		for line in content.lines() {
-			if line.contains("{") && line.chars().filter(|c| !c.is_whitespace() && *c != '{').count() > 0 {
+			if line.contains("{") && line.chars().filter(|c| !c.is_whitespace()).count() > 1 {
 				errors.push(format!("[{}:{}]Opening brace must be on their own line.", filename, line_number));
 			}
-			else if line.contains("}") && line.chars().filter(|c| !c.is_whitespace() && *c != '}' && *c != ';').count() > 0 {
-				errors.push(format!("[{}:{}]Closing brace must be on their own line.", filename, line_number));
+			else if line.contains("}") {
+				let nb_non_white_char =  line.chars().filter(|c| !c.is_whitespace()).count();
+				if !(nb_non_white_char == 1 || (nb_non_white_char == 2 && line.contains("};"))) {
+					errors.push(format!("[{}:{}]Closing brace must be on their own line.", filename, line_number));
+				}
 			}
 
 			line_number += 1;
@@ -79,5 +82,46 @@ impl Rule for IndentationLevel {
 		}
 
 		return errors;
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	#[test]
+	fn own_line_brace() {
+		let own_line_brace = OwnLineBrace::new();
+
+		assert_eq!(own_line_brace.verify("", "{\nsome text\n}\n").len(), 0);
+		assert_eq!(own_line_brace.verify("", "  {  ").len(), 0);
+		assert_eq!(own_line_brace.verify("", "};").len(), 0);
+
+		assert_ne!(own_line_brace.verify("", "{}\n").len(), 0);
+		assert_ne!(own_line_brace.verify("", "}}").len(), 0);
+		assert_ne!(own_line_brace.verify("", "{{").len(), 0);
+
+		assert_eq!(own_line_brace.verify("", "{some").len(), 1);
+		assert_eq!(own_line_brace.verify("", ";}").len(), 1);
+		assert_eq!(own_line_brace.verify("", "} ;").len(), 1);
+
+	}
+
+	#[test]
+	fn indentation_level() {
+		let indentation_level = IndentationLevel::new(4);
+
+		assert_eq!(indentation_level.verify("", "             ").len(), 0);
+		assert_eq!(indentation_level.verify("", "{\n    \n}   ").len(), 0);
+		assert_eq!(indentation_level.verify("", "{\n    {\n    }\n}   ").len(), 0);
+
+		//Missing closing and opening brace
+		assert_eq!(indentation_level.verify("", "{").len(), 1);
+		assert_eq!(indentation_level.verify("", "}").len(), 1);
+
+		//Wrong indentation
+		assert_eq!(indentation_level.verify("", "{\n 56\n}").len(), 1);
+		assert_eq!(indentation_level.verify("", "{\n     56\n}").len(), 1);
+		assert_eq!(indentation_level.verify("", "{\n    {\n  }\n}   ").len(), 1);
+		assert_eq!(indentation_level.verify("", "{\n    {\n    test\n    }\n}   ").len(), 1);
 	}
 }
