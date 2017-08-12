81ce8ecc3840ad71c151303f0556abc330f872aa
Jeremy Beuvry <jeremy.beuvry@epita.fr>
2017-08-12 11:34:15 +0200

    fix

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -3,15 +3,12 @@ pub mod brace;
 pub mod misc;
 pub mod preprocessor;
 pub mod function;
-pub mod naming;
 
 pub use self::indentation::*;
 pub use self::brace::*;
 pub use self::misc::*;
 pub use self::preprocessor::*;
 pub use self::function::*;
-pub use self::naming::*;
-
 
 pub trait Rule {
 	fn verify(&self, filename: &str, content: &str) -> Vec<String>;
